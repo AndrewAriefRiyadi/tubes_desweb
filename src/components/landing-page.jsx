b159d050ddd4aee1384bc5c586ddeec39e461116
AndrewAriefRiyadi <andrewariefr@gmail.com>
2023-11-11 18:21:23 +0800

    fix tailwinds dan folder components

---
+++ src/components/landing-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return( 
+        <ul className="flex space-x-4 ">
+            <li>home</li>
+            <li>contact</li>
+            <li>login</li>
+            <li>register</li>
+        </ul>
+    );
+    }
+export default Navbar;
